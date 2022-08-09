1101f644db2ac5ed1c6b0d50106cb88b4fc1fb0e
Bluefox182 <fergaldoss@gmail.com>
2022-08-09 10:37:29 -0500

    add formspree api

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,62 +1,84 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
+import React, { useState } from "react";
+import styled from "styled-components";
+import { useForm, ValidationError } from "@formspree/react";
 
 function Contact() {
+  const [state, handleSubmit] = useForm("mwkzkezo");
+  if (state.succeeded) {
+    return (
+      <h3
+        style={{
+          textAlign: "center",
+          fontSize: "28px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
+        Thanks for contact me! I will answer as soon as possible.
+      </h3>
+    );
+  }
   return (
     <StyledContact>
-      <div className='ContactForm-Container'>
-        <div className='contact-content'>
+      <div className="ContactForm-Container">
+        <div className="contact-content">
           <div>
-            <h1 className='title'>Let's Talk</h1>
-            <h2 className='paragraph-text'>
+            <h1 className="title">Let's Talk</h1>
+            <h2 className="paragraph-text">
               Whether it's just to say Hi 👋 or if you have a question or
               proposal, send me a message!
             </h2>
           </div>
           <div>
             <form
-              method='POST'
-              name='contact'
-              netlify-honeypot='bot-field'
-              data-netlify='true'
-              className='form'
-              action='/pages/contact'
+              method="POST"
+              name="contact"
+              className="form"
+              action="https://formspree.io/f/mwkzkezo"
+              onSubmit={handleSubmit}
             >
-              <input type='hidden' name='form-name' value='contact' />
-              <div className='form-container'>
-                <div className='form-field'>
+              <input type="hidden" name="form-name" value="contact" />
+              <div className="form-container">
+                <div className="form-field">
                   <input
-                    className='form-input'
-                    type='text'
-                    id='name'
-                    name='name'
-                    placeholder='Name'
+                    className="form-input"
+                    type="text"
+                    id="name"
+                    name="name"
+                    placeholder="Name"
                     required
                   />
+                  <ValidationError
+                    prefix="Name"
+                    field="name"
+                    errors={state.errors}
+                  />
                 </div>
-                <div className='form-field'>
+                <div className="form-field">
                   <input
-                    className='form-input'
-                    type='email'
-                    id='email'
-                    name='email'
-                    placeholder='Email'
+                    className="form-input"
+                    type="email"
+                    id="email"
+                    name="email"
+                    placeholder="Email"
                     required
                   />
                 </div>
               </div>
-              <div className='form-textarea'>
+              <div className="form-textarea">
                 <textarea
-                  name='message'
-                  id='message'
-                  cols='30'
-                  rows='10'
-                  placeholder='Your Message'
+                  name="message"
+                  id="message"
+                  cols="30"
+                  rows="10"
+                  placeholder="Your Message"
                   required
                 ></textarea>
               </div>
-              <div className='form-button'>
-                <button className='button-contact' type='submit' value='Send'>
+              <div className="form-button">
+                <button className="button-contact" type="submit" value="Send">
                   Send Message!
                 </button>
               </div>
@@ -158,7 +180,7 @@ const StyledContact = styled.div`
     font-size: 1.2rem;
     font-weight: 300;
     color: var(--font-light-color-2);
-    font-family: 'Montserrat', sans-serif;
+    font-family: "Montserrat", sans-serif;
 
     &:focus {
       outline: none;
@@ -179,7 +201,7 @@ const StyledContact = styled.div`
     width: 100%;
     padding: 0.8rem 1rem;
     border-radius: 8px;
-    font-family: 'Montserrat', sans-serif;
+    font-family: "Montserrat", sans-serif;
     ::placeholder {
       color: var(--font-light-color-2);
       opacity: 1; /* Firefox */
@@ -200,7 +222,7 @@ const StyledContact = styled.div`
     justify-content: center;
     align-items: center;
     border-radius: 8px;
-    font-family: 'Montserrat', sans-serif;
+    font-family: "Montserrat", sans-serif;
     cursor: pointer;
     :hover {
       background: var(--primary-color-light);
