34938acc8fdb50dfd1b57a9683ed6cada44ea692
Bluefox182 <fergaldoss@gmail.com>
2022-09-21 02:14:10 -0500

    add new card project

--- src/data/projects.js
+++ src/data/projects.js
@@ -4,29 +4,20 @@ import React01 from "../assets/img/ImgProjects/cupcakelanding.png";
 import React02 from "../assets/img/ImgProjects/project06.png";
 import React03 from "../assets/img/ImgProjects/weather.png";
 import React04 from "../assets/img/ImgProjects/rickandmorty.png";
+import Jowells from "../assets/img/ImgProjects/jowells.png";
 
 const projects = [
   {
     id: 1,
-    category: "CSS3",
-    image: CSS01,
-    link1: "https://github.com/Bluefox182/Google-Clone",
-    link2: "https://bluefox182.github.io/Google-Clone/",
-    title: "Google Clone",
-    text: "My own version of Google's homepage made with HTML and pure CSS.",
+    category: "ReactJS",
+    image: Jowells,
+    // link1: "https://github.com/Bluefox182/ultimate-rick-and-morty",
+    link2: "https://jowells-barber-shop.vercel.app/",
+    title: "Jowell's Barbershop",
+    text: "A special project for a local barbershop to showcase their work, made with React.",
   },
-
   {
     id: 2,
-    category: "JavaScript",
-    image: JS01,
-    link1: "https://github.com/Bluefox182/JavaScript-Drum-Kit",
-    link2: "https://bluefox182.github.io/JavaScript-Drum-Kit/",
-    title: "Drum Kit",
-    text: "Play the drums with your keyboard, this games was built with Js Vanilla.",
-  },
-  {
-    id: 3,
     category: "JavaScript",
     image: React01,
     link1: "https://github.com/Bluefox182/cupcake-landing-page",
@@ -34,9 +25,8 @@ const projects = [
     title: "Cupcake Landing Page",
     text: "A landing page for a cupcake shop, made with React.",
   },
-
   {
-    id: 4,
+    id: 3,
     category: "ReactJS",
     image: React02,
     // link1: "",
@@ -45,7 +35,7 @@ const projects = [
     text: "A special Project for a customer who wanted a website for his event company, made with React.",
   },
   {
-    id: 5,
+    id: 4,
     category: "ReactJS",
     image: React03,
     link1: "https://github.com/Bluefox182/ultimate-weather-app",
@@ -54,13 +44,31 @@ const projects = [
     text: "A beautiful weather app built with ReactJS and OpenWeather API",
   },
   {
-    id: 6,
+    id: 5,
     category: "ReactJS",
     image: React04,
     link1: "https://github.com/Bluefox182/ultimate-rick-and-morty",
     link2: "https://bluefox-rick-and-morty.vercel.app/",
     title: "Rick and Morty API",
     text: "Discover the characters of Rick and Morty with this app built with ReactJS and Rick and Morty API",
+  },
+  {
+    id: 6,
+    category: "CSS3",
+    image: CSS01,
+    link1: "https://github.com/Bluefox182/Google-Clone",
+    link2: "https://bluefox182.github.io/Google-Clone/",
+    title: "Google Clone",
+    text: "My own version of Google's homepage made with HTML and pure CSS.",
+  },
+  {
+    id: 2,
+    category: "JavaScript",
+    image: JS01,
+    link1: "https://github.com/Bluefox182/JavaScript-Drum-Kit",
+    link2: "https://bluefox182.github.io/JavaScript-Drum-Kit/",
+    title: "Drum Kit",
+    text: "Play the drums with your keyboard, this games was built with Js Vanilla.",
   },
 ];
 
